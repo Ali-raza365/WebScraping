43ec9cea9df43528f1034fcd6dfb668ea3d0399a
Haider <59225711+Ali-raza365@users.noreply.github.com>
2023-05-14 23:53:02 +0500

    update file path

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.post("/screenshot", async (req, res) => {
           await page.goto(url, { timeout:100000 });
           // await page.waitForLoadState('networkidle');
           const filePath = path.join(__dirname, './page.png');
-          const screenshot = await page.screenshot({  path: filePath, fullPage: true, type: "png", encoding: 'binary' , });
+          const screenshot = await page.screenshot({  path: './page.png', fullPage: true, type: "png", encoding: 'binary' , });
 
           // const imageData = fs.readFileSync('./page.png');
 
